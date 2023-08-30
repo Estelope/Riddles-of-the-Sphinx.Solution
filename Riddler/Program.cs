16ee210c36b7fe6105d5bc74333648f35efaa354
Estelope <elopez14567@gmail.com>
2023-08-29 23:07:52 -0600

    barebone ui WIP

--- Riddler/Program.cs
+++ Riddler/Program.cs
@@ -8,6 +8,15 @@ namespace ShapeTracker
     static void Main()
     {
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to your Doom....");
+      Console.WriteLine("I am the Riddle Sphinx...");
+      Console.WriteLine("Answer my question correctly to survive D:");
+      Console.WriteLine("Are you ready to begin? Enter 'yes' to proceed, or 'no' to exit the console");
+      string userInput = Console.ReadLine();
+      if (userInput == "yes")
+      {
+        Console.WriteLine("Answer the following: \n {Riddle}");
+      }
     }
   }
 }
