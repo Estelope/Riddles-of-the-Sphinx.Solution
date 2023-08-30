f4286fe12571641fbde0dcee6885d6b3591d2969
Estelope <elopez14567@gmail.com>
2023-08-29 23:37:33 -0600

    pass second test tdd

--- Riddler.Tests/ModelTests/RiddleTests.cs
+++ Riddler.Tests/ModelTests/RiddleTests.cs
@@ -1,5 +1,4 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Riddle.Models;
 
 namespace ROTS.Tests
 {
@@ -12,6 +11,15 @@ namespace ROTS.Tests
     {
       Sphinx newRiddle = new Sphinx();
       Assert.AreEqual(typeof(Sphinx), newRiddle.GetType());
+    }
+
+    [TestMethod]
+    public void GetRandomRiddle_ReturnsValidRiddle()
+    {
+    Sphinx sphinx = new Sphinx();
+    Riddle riddle = sphinx.GetRandomRiddle();
+        Assert.IsFalse(string.IsNullOrEmpty(riddle.Question));
+        Assert.IsFalse(string.IsNullOrEmpty(riddle.Answer));
     }
 
   }
